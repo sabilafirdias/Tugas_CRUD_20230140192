7d4d6d7cd02b8daae1d0702657d69fe824cca2fb
sabilafirdias <sabilafirdias@gmail.com>
2026-03-15 09:21:17 +0700

    membuat KtpRepository

--- src/main/java/com/example/praktikum2/repository/KtpRepository.java
+++ src/main/java/com/example/praktikum2/repository/KtpRepository.java
@@ -1,10 +1,12 @@
 package com.example.praktikum2.repository;
 
-import com.example.praktikum2.model.ktp;
+import com.example.praktikum2.model.entity.Ktp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-public interface KtpRepository extends JpaRepository<ktp, Integer> {
-
-
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
 }
