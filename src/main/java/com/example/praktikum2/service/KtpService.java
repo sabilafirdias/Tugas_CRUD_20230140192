5bb2a55aaa56ad07eae8fa816d701a5306f902c1
sabilafirdias <sabilafirdias@gmail.com>
2026-03-15 09:22:27 +0700

    membuat KtpService

--- src/main/java/com/example/praktikum2/service/KtpService.java
+++ src/main/java/com/example/praktikum2/service/KtpService.java
@@ -1,5 +1,16 @@
 package com.example.praktikum2.service;
-public class KtpService {
 
-    
+import com.example.praktikum2.model.dto.KtpDto;
+import com.example.praktikum2.model.dto.KtpAddRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface KtpService {
+    KtpDto addKtp(KtpAddRequest request);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpById(Integer id);
+    KtpDto updateKtp(Integer id, KtpAddRequest request);
+    void deleteKtp(Integer id);
 }
